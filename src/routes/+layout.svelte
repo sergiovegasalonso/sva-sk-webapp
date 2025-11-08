5cfa4dbfd2f23704076bdfed24b675f927b9fa50
Sergio Vegas Alonso <sergio.vegas.alonso@gmail.com>
2025-11-08 15:32:14 +0100

    icons

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
 	import '../app.css';
 	import favicon from '$lib/assets/favicon.svg';
+	import Header from '$lib/components/header/Header.svelte';
+	import Navbar from '$lib/components/navbar/Navbar.svelte';
 
 	let { children } = $props();
 </script>
@@ -9,4 +11,6 @@
 	<link rel="icon" href={favicon} />
 </svelte:head>
 
+<Header />
+<Navbar />
 {@render children()}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,20 @@
+<script lang="ts">
+	import { setLocale } from '$lib/paraglide/runtime';
+	import { page } from '$app/state';
+	import { goto } from '$app/navigation';
+	import { m } from '$lib/paraglide/messages.js';
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<h1>{m.hello_world({ name: 'SvelteKit User' })}</h1>
+<div>
+	<button onclick={() => setLocale('en')}>en</button>
+	<button onclick={() => setLocale('es')}>es</button>
+</div>
+<p>
+	If you use VSCode, install the <a
+		href="https://marketplace.visualstudio.com/items?itemName=inlang.vs-code-extension"
+		target="_blank">Sherlock i18n extension</a
+	> for a better i18n experience.
+</p>
