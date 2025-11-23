ebf8102c0b6ba8bb5f10f0117632bec39c603165
Sergio Vegas Alonso <sergio.vegas.alonso@gmail.com>
2025-11-23 17:48:04 +0100

    toogle theme ok

--- src/lib/components/atoms/buttons/theme-button/ThemeButton.svelte
+++ src/lib/components/atoms/buttons/theme-button/ThemeButton.svelte
@@ -1,21 +1,31 @@
 <script lang="ts">
+	import { browser } from '$app/environment';
 	import Button from '$lib/components/atoms/buttons/Button.svelte';
 	import Moon from '$lib/components/atoms/icons/Moon.svelte';
 	import Sun from '$lib/components/atoms/icons/Sun.svelte';
 	import Space from '$lib/components/atoms/Space.svelte';
 	import { Theme } from '$lib/domain/enums/theme.enum';
 
+	let isDarkThemeActive = $state(
+		browser && document.documentElement.classList.contains(Theme.Dark)
+	);
+
 	function toggleTheme() {
-		document.documentElement.classList.toggle(Theme.Dark);
-		localStorage.theme = document.documentElement.classList.contains(Theme.Dark)
-			? Theme.Dark
-			: Theme.Light;
+		const documentElement = document.documentElement;
+		documentElement.classList.toggle(Theme.Dark);
+
+		const documentElementContainsDarkClass = documentElement.classList.contains(Theme.Dark);
+		isDarkThemeActive = documentElementContainsDarkClass;
+		localStorage.theme = documentElementContainsDarkClass ? Theme.Dark : Theme.Light;
 	}
 </script>
 
-<Button ariaLabel="Change language" onclick={toggleTheme}>
-	<Sun />
-	<Moon />
+<Button ariaLabel="Toggle theme" onclick={toggleTheme}>
+	{#if isDarkThemeActive}
+		<Sun />
+	{:else}
+		<Moon />
+	{/if}
 	<Space />
-	Theme
+	<span>Theme</span>
 </Button>
