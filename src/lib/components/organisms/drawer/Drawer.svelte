08e72eb795639367f446067f08510fb556abb921
Sergio Vegas Alonso <sergio.vegas.alonso@gmail.com>
2025-11-27 21:55:52 +0100

    ok

--- src/lib/components/organisms/drawer/Drawer.svelte
+++ src/lib/components/organisms/drawer/Drawer.svelte
@@ -3,8 +3,10 @@
 </script>
 
 <nav
-	class="bg fixed top-0 right-0 bottom-0 z-80 w-96 transform-gpu border-2 bg-l transition-transform duration-500 ease-in-out dark:bg-d {drawerState.open
-		? ''
-		: 'translate-x-full'}">
+	class="fixed top-auto right-0 bottom-0 left-0 z-80 flex h-96 w-auto transform-gpu border-2 bg-l transition-transform
+	       duration-500 ease-in-out lg:top-0 lg:left-auto lg:h-auto lg:w-96 dark:bg-d
+		   {!drawerState.open
+		? 'translate-x-0 translate-y-full lg:translate-x-full lg:translate-y-0'
+		: ''}">
 	<ul></ul>
 </nav>
