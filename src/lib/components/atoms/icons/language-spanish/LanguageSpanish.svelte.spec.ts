4ab470708620f66c081546a82ed28f3ca95f77d2
Sergio Vegas Alonso <sergio.vegas.alonso@gmail.com>
2025-12-06 22:54:50 +0100

    icon tests added

---
+++ src/lib/components/atoms/icons/language-spanish/LanguageSpanish.svelte.spec.ts
@@ -0,0 +1,31 @@
+import { describe, expect, it } from 'vitest';
+import { render } from 'vitest-browser-svelte';
+import LanguageSpanish from './LanguageSpanish.svelte';
+
+const DEFAULT_CLASSES = ['size-6'];
+
+const renderButton = (props = {}) => {
+	return render(LanguageSpanish, {
+		...props
+	});
+};
+
+describe('/LanguageSpanish.svelte', () => {
+	it('should render only one svg element', async () => {
+		const { container } = renderButton();
+		const svgElements = container.querySelectorAll('svg');
+		expect(svgElements.length).toBe(1);
+	});
+
+	it('should render svg with default classes', async () => {
+		const { container } = renderButton();
+		const svg = container.querySelector('svg');
+		expect(svg?.classList.toString()).toBe(`${DEFAULT_CLASSES.join(' ')} `);
+	});
+
+	it('should render svg with default and custom classes', async () => {
+		const { container } = renderButton({ class: 'z-1000' });
+		const svg = container.querySelector('svg');
+		expect(svg?.classList.toString()).toBe(`${DEFAULT_CLASSES.join(' ')} z-1000`);
+	});
+});
---
+++ src/lib/components/atoms/icons/language/Language.svelte.spec.ts
@@ -0,0 +1,31 @@
+import { describe, expect, it } from 'vitest';
+import { render } from 'vitest-browser-svelte';
+import Language from './Language.svelte';
+
+const DEFAULT_CLASSES = ['size-6'];
+
+const renderButton = (props = {}) => {
+	return render(Language, {
+		...props
+	});
+};
+
+describe('/Language.svelte', () => {
+	it('should render only one svg element', async () => {
+		const { container } = renderButton();
+		const svgElements = container.querySelectorAll('svg');
+		expect(svgElements.length).toBe(1);
+	});
+
+	it('should render svg with default classes', async () => {
+		const { container } = renderButton();
+		const svg = container.querySelector('svg');
+		expect(svg?.classList.toString()).toBe(`${DEFAULT_CLASSES.join(' ')} `);
+	});
+
+	it('should render svg with default and custom classes', async () => {
+		const { container } = renderButton({ class: 'z-1000' });
+		const svg = container.querySelector('svg');
+		expect(svg?.classList.toString()).toBe(`${DEFAULT_CLASSES.join(' ')} z-1000`);
+	});
+});
