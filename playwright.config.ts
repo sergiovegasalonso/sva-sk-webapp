907113bdd57145b797097873350c39201a8e7823
Sergio Vegas Alonso <sergio.vegas@col.vueling.com>
2026-01-15 18:04:23 +0100

    .svelte-kit folder uploaded after build (3)

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,8 +1,11 @@
 import { defineConfig } from '@playwright/test';
 
+const shouldSkipBuild = process.env.SKIP_BUILD === '1';
+const command = shouldSkipBuild ? 'pnpm run preview' : 'pnpm run build && pnpm run preview';
+
 export default defineConfig({
 	webServer: {
-		command: 'pnpm run build && pnpm run preview',
+		command,
 		port: 4173
 	},
 	testDir: 'tests/e2e'
